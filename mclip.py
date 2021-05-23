26ccc7b2531741fe8c5c4675d9698ddc658b788c
Andrew Dotterer <adotterer@gmail.com>
2021-05-23 16:40:34 -0700

    dictionary email

---
+++ mclip.py
@@ -0,0 +1,4 @@
+TEXT = {'agree': """Yes, I am available to play. Thank you for contacting me!""",
+        'busy': """Sorry, I already have another commitment at that time.""",
+        'beggar': """Assuming this is a paid opporunity-- what is your rate?""",
+        'upsell': """Would you consider """}
