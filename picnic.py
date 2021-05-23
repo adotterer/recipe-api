25a2a2781783f4b5c6b3af528763a815bce63e1b
Andrew Dotterer <adotterer@gmail.com>
2021-05-23 13:18:06 -0700

    python linter nm

--- picnic.py
+++ picnic.py
@@ -1,6 +1,7 @@
 def printPicnic(itemsDict, leftWidth, rightWidth):
-  print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
-  for k, v in itemsDict.items():
-    print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidht))
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
 
-picnicItems = {}
+    # for k, v in itemsDict.items():
+    # print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidht))
+printPicnic({}, 12, 12)
+# picnicItems = {'sandwiches': 4, 'apples': 12, 'cups':4, 'cookies':8000}
