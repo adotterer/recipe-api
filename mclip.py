d53e7d15e6b7fb1af25a4cbd2b34c6d93ef49b87
Andrew Dotterer <adotterer@gmail.com>
2021-05-23 16:45:30 -0700

    sys.argv

--- mclip.py
+++ mclip.py
@@ -1,4 +1,12 @@
+import sys
 TEXT = {'agree': """Yes, I am available to play. Thank you for contacting me!""",
-        'busy': """Sorry, I already have another commitment at that time.""",
+        'busy': """I'm sorry, I already have another commitment at that time.""",
         'beggar': """Assuming this is a paid opporunity-- what is your rate?""",
         'upsell': """Would you consider """}
+
+
+if len(sys.argv) < 2:
+    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+    sys.exit()
+
+keyphrase = sys.argv[1]       # first command line arg is the keyphrase
