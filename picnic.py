c57d37ea6a314d46a0872bc5ce0b06fe44cb788b
Andrew Dotterer <adotterer@gmail.com>
2021-05-23 13:50:31 -0700

    rjust ljust

--- picnic.py
+++ picnic.py
@@ -1,7 +1,12 @@
 def printPicnic(itemsDict, leftWidth, rightWidth):
     print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+# the second argument of rjust & ljust fills the remaining characters with filler
+# must be one character long
 
-    # for k, v in itemsDict.items():
-    # print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidht))
-printPicnic({}, 12, 12)
-# picnicItems = {'sandwiches': 4, 'apples': 12, 'cups':4, 'cookies':8000}
+
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+printPicnic(picnicItems, 12, 12)
+
+print("hello".rjust(30, "* * "))
