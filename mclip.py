fd5fa8d3e9959806c682567946dba1bfd3541f36
Andrew Dotterer <adotterer@gmail.com>
2021-05-23 19:30:23 -0700

    copy

--- mclip.py
+++ mclip.py
@@ -1,4 +1,8 @@
+#! python3
+# mclip.py - A multi-clipboard program.
+
 import sys
+import pyperclip
 TEXT = {'agree': """Yes, I am available to play. Thank you for contacting me!""",
         'busy': """I'm sorry, I already have another commitment at that time.""",
         'beggar': """Assuming this is a paid opporunity-- what is your rate?""",
@@ -10,3 +14,9 @@ if len(sys.argv) < 2:
     sys.exit()
 
 keyphrase = sys.argv[1]       # first command line arg is the keyphrase
+
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+    print('There is no text for ' + keyphrase)
