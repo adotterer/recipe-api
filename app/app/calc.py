241902346dbf7a984372c53344240d90ca585582
Andrew Dotterer <adotterer@gmail.com>
2021-09-28 08:49:49 -0700

    remove calc, tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,2 +1,6 @@
 def add(x, y):
     return x + y
+
+
+def subtract(x, y):
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, subtract
 
 
 class CalcTests(TestCase):
@@ -8,3 +8,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that two numbers are added together"""
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+
+        self.assertEqual(subtract(5, 11), 6)
