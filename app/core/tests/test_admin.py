a8504ba4873bbfe772238b12f67c3a831bab6dbe
Andrew Dotterer <adotterer@gmail.com>
2021-09-28 15:22:56 -0700

    test for if user is listed

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -17,3 +17,11 @@ class AdminSiteTests(TestCase):
             password="password123",
             name="Test user full name"
         )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
