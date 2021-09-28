fccaa0b9f0397bc907072e7d52df970d62c74737
Andrew Dotterer <adotterer@gmail.com>
2021-09-28 09:36:23 -0700

    user must have an email address

--- app/core/models.py
+++ app/core/models.py
@@ -7,6 +7,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
+        if not email:
+            raise ValueError("Users must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
