af7b49ee1546371943b856ae5ee6a59181e4a953
Andrew Dotterer <adotterer@gmail.com>
2021-09-28 12:42:43 -0700

    login super user

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user, get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@adotterer.com",
+            password="password123"
+        )
+        self.client.force_login(self.admin_user)
